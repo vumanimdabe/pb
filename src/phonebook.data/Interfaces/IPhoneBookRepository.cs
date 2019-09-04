2b9961f1cfaade918446d124ae77bf56b2deea77
Vumani Mdabe <vumanimdabe@gmail.com>
2019-09-04 15:13:38 +0200

    added repositry class interface

---
+++ src/phonebook.data/Interfaces/IPhoneBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using phonebook.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phonebook.data.Repos
+{
+    public interface IPhoneBookRepository
+    {
+        Task<IEnumerable<PhoneBookEntry>> GetEntriesByNameAsync(string name);
+        Task<IEnumerable<PhoneBookEntry>> GetEntriesByPhoneNumberAsync(string phoneNumber);
+        Task addEntryAsync(PhoneBookEntry entry);
+    }
+}
---
+++ src/phonebook.data/Models/PhoneBookEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace phonebook.data.Models
+{
+    public class PhoneBookEntry
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
