392bdfd8dde3aa599d77b6fff354737779f5b1e4
Vumani Mdabe <vumanimdabe@gmail.com>
2019-09-06 16:04:07 +0200

    Added Sql Dump files

---
+++ SQL/Dump20190906/PhoneBookDb_PhoneBookEntry.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 104.197.214.107    Database: PhoneBookDb
+-- ------------------------------------------------------
+-- Server version	5.7.14-google
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `PhoneBookEntry`
+--
+
+DROP TABLE IF EXISTS `PhoneBookEntry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `PhoneBookEntry` (
+  `Name` varchar(45) NOT NULL,
+  `PhoneNumber` varchar(45) NOT NULL,
+  PRIMARY KEY (`Name`,`PhoneNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PhoneBookEntry`
+--
+
+LOCK TABLES `PhoneBookEntry` WRITE;
+/*!40000 ALTER TABLE `PhoneBookEntry` DISABLE KEYS */;
+INSERT INTO `PhoneBookEntry` VALUES ('Jason Borne','+2707255128837'),('Samkelo','+27655512900'),('Vumani','+27714402514'),('Vumani Mabe','+270715521168');
+/*!40000 ALTER TABLE `PhoneBookEntry` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-06 16:02:20
